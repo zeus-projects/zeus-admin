0524df4a2f2848860635597a12e2f24215d02446
alexchen <1274812218@qq.com>
2023-07-05 21:22:17 +0800

    feat: 魔化

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,11 +5,11 @@ module.exports = {
       2,
       'always',
       [
-        'feat', // 新功能(feature)
-        'fix', // 修补bug
-        'docs', // 文档(documentation)
-        'style', // 格式、样式(不影响代码运行的变动)
-        'refactor', // 重构(即不是新增功能，也不是修改BUG的代码)
+        'feat', // 新功能 (feature)
+        'fix', // 修补 bug
+        'docs', // 文档 (documentation)
+        'style', // 格式、样式 (不影响代码运行的变动)
+        'refactor', // 重构 (即不是新增功能，也不是修改 BUG 的代码)
         'perf', // 优化相关，比如提升性能、体验
         'test', // 添加测试
         'ci', // 持续集成修改
